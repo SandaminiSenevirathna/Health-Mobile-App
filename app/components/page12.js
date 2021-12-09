b86013b8aa829dcd389643799349e33ee50a41f0
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-09 14:54:25 +0530

    add back button to the page12

--- app/components/page12.js
+++ app/components/page12.js
@@ -19,6 +19,14 @@ const page12 = ({navigation}) => {
         {' '}
         * Your body needs exercises at least one hour per day.{' '}
       </Text>
+      <View style={styles.button1}>
+        <Button
+          fontColor="#000000"
+          color="#306754"
+          title="Back"
+          onPress={() => navigation.navigate('page9')}
+        />
+      </View>
     </View>
   );
 };
@@ -62,6 +70,20 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     letterSpacing: 0.34,
     lineHeight: 40,
+  },
+  button1: {
+    fontWeight: 'bold',
+    fontColor: '#000000',
+    margin: 15,
+    marginTop: 10,
+    fontSize: 20,
+    marginLeft: -1,
+    elevation: 8,
+    backgroundColor: '#033E3E',
+    borderRadius: 20,
+    width: 190,
+    paddingVertical: 20,
+    paddingHorizontal: 15,
   },
 });
 export default page12;
