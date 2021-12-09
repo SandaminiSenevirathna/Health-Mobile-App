d05b82a5cdeb92a0eef84a7390f28bffee1bc31d
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-09 11:03:20 +0530

    add images for page8

--- app/components/page8.js
+++ app/components/page8.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button, Image} from 'react-native';
 
 const page8 = ({navigation}) => {
   return (
     <View style={styles.view}>
+      <View style={styles.image9}>
+        <Image source={require('../assets/images/highscare.png')} />
+      </View>
       <View />
+      <View style={styles.image10}>
+        <Image source={require('../assets/images/sugar.png')} />
+      </View>
       <Text style={styles.Text1}> 200 To Above</Text>
     </View>
   );
@@ -14,10 +20,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
   },
+  image9: {
+    width: 85,
+    height: 45,
+    marginTop: 110,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 130,
+  },
+  image10: {
+    width: 85,
+    height: 45,
+    marginTop: 160,
+    alignItems: 'center',
+    paddingLeft: 120,
+  },
   Text1: {
     alignItems: 'center',
     fontSize: 36,
     color: 'black',
+    marginTop: -220,
     marginBottom: 50,
     letterSpacing: 0.34,
     lineHeight: 40,
