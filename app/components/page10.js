25959cb84c541704773cbd5b9ea3dfacd122321b
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-09 13:26:58 +0530

    add image to the page 10

--- app/components/page10.js
+++ app/components/page10.js
@@ -4,6 +4,9 @@ import {View, Text, StyleSheet, Button, Image} from 'react-native';
 const page10 = ({navigation}) => {
   return (
     <View style={styles.view}>
+      <View style={styles.Cho1}>
+        <Image source={require('../assets/images/happy.png')} />
+      </View>
       <View />
       <Text style={styles.Text1}> 0 - 200</Text>
     </View>
@@ -14,14 +17,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
   },
+  Cho1: {
+    width: 85,
+    height: 45,
+    marginTop: 75,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 130,
+  },
   Text1: {
     alignItems: 'center',
     fontSize: 36,
+    marginTop: -20,
     color: 'black',
-    marginBottom: 50,
     letterSpacing: 0.34,
     lineHeight: 40,
-    paddingLeft: 225,
+    marginLeft: 250,
   },
 });
 export default page10;
