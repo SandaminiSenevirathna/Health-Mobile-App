5209656b8e33d750b2af89a2eced1747d297e66d
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-09 12:08:34 +0530

    add 0-200 button

--- app/components/page9.js
+++ app/components/page9.js
@@ -5,6 +5,14 @@ const page9 = ({navigation}) => {
   return (
     <View style={styles.view}>
       <Text style={styles.Text1}> Select Your Cholesterol Level </Text>
+      <View style={styles.button}>
+        <Button
+          color="#006400"
+          fontColor="#000000"
+          title="0  -  200"
+          //onPress={() => navigation.navigate('page5')}
+        />
+      </View>
     </View>
   );
 };
@@ -12,16 +20,26 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     backgroundColor: '#FFFFFF',
-    justifyContent: 'center',
   },
   Text1: {
     alignItems: 'center',
     fontSize: 34,
-    color: 'black',
+    color: '#000000',
     marginBottom: 1,
     letterSpacing: 0.34,
     lineHeight: 40,
     paddingLeft: 50,
+  },
+  button: {
+    fontWeight: 'bold',
+    fontColor: '#000000',
+    margin: 25,
+    fontSize: 20,
+    elevation: 8,
+    backgroundColor: '#006400',
+    borderRadius: 70,
+    paddingVertical: 50,
+    paddingHorizontal: 45,
   },
 });
 export default page9;
