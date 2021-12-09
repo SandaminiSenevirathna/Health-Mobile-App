1d04b94b5dcc53800938f21a1bb865cf164a0fa4
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-09 14:11:35 +0530

    add image to the page11

--- app/components/page11.js
+++ app/components/page11.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 const page11 = ({navigation}) => {
   return (
     <View style={styles.view}>
       <View />
+      <View style={styles.Cho1}>
+        <Image source={require('../assets/images/high.png')} />
+      </View>
       <Text style={styles.Text1}> 200 - 239</Text>
     </View>
   );
@@ -13,6 +16,14 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     backgroundColor: '#FFFFFF',
+  },
+  Cho1: {
+    width: 85,
+    height: 45,
+    marginTop: 110,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 130,
   },
   Text1: {
     alignItems: 'center',
