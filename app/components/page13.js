f0ffc828bc3b3d1643d97e9fa6ad0b693fd97de0
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-11 23:12:22 +0530

    add levels

--- app/components/page13.js
+++ app/components/page13.js
@@ -5,6 +5,34 @@ const page13 = ({navigation}) => {
   return (
     <View style={styles.view}>
       <Text style={styles.Text1}> Select Your Presser Level </Text>
+      <View style={styles.button}>
+        <Button
+          color="#006400"
+          title="90 - 120  /  60 - 80"
+          //onPress={() => navigation.navigate('page5')}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button
+          color="#006400"
+          title="120  /  80"
+          //onPress={() => navigation.navigate('page5')}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button
+          color="#006400"
+          title="120 - 139  /  80 - 89"
+          //onPress={() => navigation.navigate('page5')}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button
+          color="#006400"
+          title="Above 140  /  Above 90"
+          //onPress={() => navigation.navigate('page5')}
+        />
+      </View>
     </View>
   );
 };
@@ -22,6 +50,19 @@ const styles = StyleSheet.create({
     letterSpacing: 0.34,
     lineHeight: 40,
     paddingLeft: 50,
+  },
+  button: {
+    fontWeight: 'bold',
+    fontColor: '#000000',
+    margin: 25,
+    fontSize: 20,
+    elevation: 8,
+    backgroundColor: '#006400',
+    borderRadius: 18,
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
 });
 export default page13;
