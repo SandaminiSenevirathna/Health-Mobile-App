e202da3b5f956004b3958e368f92ab3e04c310a6
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-14 23:49:29 +0530

    add margin top to the page 16

--- app/components/page16.js
+++ app/components/page16.js
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontColor: '#000000',
     margin: 15,
-    marginTop: 10,
+    marginTop: 50,
     fontSize: 20,
     marginLeft: -1,
     elevation: 8,
