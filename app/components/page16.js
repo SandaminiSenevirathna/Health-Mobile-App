7f57ec60c8bd73f39e8dcb171a54552fd43e8086
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-14 23:26:17 +0530

    add image for presser interface 3

--- app/components/page16.js
+++ app/components/page16.js
@@ -5,6 +5,9 @@ const page16 = ({navigation}) => {
   return (
     <View style={styles.view}>
       <View />
+      <View style={styles.pre3}>
+        <Image source={require('../assets/images/high.png')} />
+      </View>
       <Text style={styles.Text1}> 120-139 / 80-89</Text>
       <View style={styles.button1}>
         <Button
@@ -29,6 +32,14 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     backgroundColor: '#FFFFFF',
+  },
+  pre3: {
+    width: 85,
+    height: 45,
+    marginTop: 110,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 130,
   },
   Text1: {
     alignItems: 'center',
