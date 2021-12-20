fc9d248ab74588642b4299d3e7e403b37dc3e5b8
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-20 13:22:45 +0530

    add some instructions to the heart window

--- app/components/page20.js
+++ app/components/page20.js
@@ -4,9 +4,16 @@ import {View, Text, StyleSheet, Button, Image} from 'react-native';
 const page20 = ({navigation}) => {
   return (
     <View style={styles.view}>
-      <View style={styles.image3}>
-        <Image source={require('../assets/images/women3.jpg')} />
-      </View>
+      <Text style={styles.Text1}> How to protect our heart?. </Text>
+      <Text style={styles.Text3}> * Eat Healthy. </Text>
+      <Text style={styles.Text3}> * Get Active. </Text>
+      <Text style={styles.Text3}> * Stay at a healthy weight. </Text>
+      <Text style={styles.Text3}> * Quit smocking. </Text>
+      <Text style={styles.Text3}>
+        {' '}
+        * Control your cholesterol and blood pressure.{' '}
+      </Text>
+      <Text style={styles.Text3}> * Manage Stress. </Text>
       <View />
     </View>
   );
@@ -16,11 +23,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
   },
-  image3: {
-    marginTop: 180,
+  Text1: {
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingLeft: 185,
+    fontSize: 26,
+    color: 'black',
+    letterSpacing: 0.34,
+    lineHeight: 40,
+    fontWeight: 'bold',
+  },
+  Text3: {
+    alignItems: 'center',
+    fontSize: 24,
+    color: 'black',
+    letterSpacing: 0.34,
+    lineHeight: 40,
+    marginTop: 10,
   },
 });
 export default page20;
