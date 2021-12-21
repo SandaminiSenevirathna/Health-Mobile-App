2a2bfcde02153b3a30fa1be84739d627a39f130a
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-12-21 17:00:13 +0530

    add new cholesterol interface

--- app/components/page10.js
+++ app/components/page10.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import {View, Text, StyleSheet, Button, Image} from 'react-native';
 
@@ -75,6 +76,68 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     letterSpacing: 0.34,
     lineHeight: 40,
+  },
+  button2: {
+    fontWeight: 'bold',
+    fontColor: '#000000',
+    margin: 15,
+    fontSize: 20,
+    marginTop: -90,
+    elevation: 8,
+    backgroundColor: '#033E3E',
+    borderRadius: 20,
+    marginLeft: 250,
+    width: 190,
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+  },
+});
+export default page10;
+*/
+import React from 'react';
+import {View, Text, StyleSheet, Button, Image} from 'react-native';
+import Chofirst from '../screens/cho1.js';
+
+const page10 = ({navigation}) => {
+  return (
+    <View style={styles.view}>
+      <View style={styles.Cho1}>
+        <Image source={require('../assets/images/happy.png')} />
+      </View>
+      <View />
+      <Text style={styles.Text1}> 0 - 200</Text>
+      <Chofirst />
+      <View style={styles.button2}>
+        <Button
+          fontColor="#000000"
+          color="#306754"
+          title="Exit"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  Cho1: {
+    width: 85,
+    height: 45,
+    marginTop: 75,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 130,
+  },
+  Text1: {
+    alignItems: 'center',
+    fontSize: 36,
+    color: '#000000',
+    letterSpacing: 0.34,
+    lineHeight: 40,
+    marginLeft: 250,
   },
   button2: {
     fontWeight: 'bold',
